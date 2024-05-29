dfd96620ed939601dd84d178dfe78ad503c8ed6c
daynlight <danielstodulski000@gmail.com>
2024-05-29 23:42:09 +0200

    Sub Projects

--- premake5.lua
+++ premake5.lua
@@ -8,7 +8,7 @@ workspace "Maze-AI"
     cppdialect "C++20"
     architecture "x86_64"
     location "Maze-AI"
-	startproject "App"
+	startproject "APP"
     objdir ("bin")
 
 -- [Modes] --
@@ -17,17 +17,16 @@ configurations { "Debug", "Release" }
 -- [Globals] --
     includedirs "Maze-AI/Lib/Include"
     libdirs "Maze-AI/Lib/Lib"
-    links {}
 
 -- [APP] --
-project "App"  
+project "APP"  
     files { "**.h", "**.cpp" } 
-    location "Maze-AI/Code"
-
+    location "Maze-AI/APP"
+    links { "AI", "MAZE" }
+    kind "consoleapp"  
 -- [AppMode Debug]
     filter "configurations:Debug"
     defines { "CE_DEBUG" }
-    kind "consoleapp"  
     symbols "On"
     optimize "Off"
     targetdir "Output/Debug" 
@@ -35,7 +34,46 @@ project "App"
 -- [AppMode Release]
     filter "configurations:Release"
     defines { "CE_RELEASE" }
-    kind "consoleapp"
+    symbols "Off"
+    optimize "On"
+    targetdir "Output/Release" 
+    debugdir "Output/Release"
+
+-- [AI] --
+project "AI"  
+    files { "**.h", "**.cpp" } 
+    location "Maze-AI/AI"
+    kind "staticlib"
+    -- [AppMode Debug]
+    filter "configurations:Debug"
+    defines { "CE_DEBUG" }
+    symbols "On"
+    optimize "Off"
+    targetdir "Output/Debug" 
+    debugdir "Output/Debug"
+    -- [AppMode Release]
+    filter "configurations:Release"
+    defines { "CE_RELEASE" }
+    symbols "Off"
+    optimize "On"
+    targetdir "Output/Release" 
+    debugdir "Output/Release"
+
+-- [MAZE] --
+project "MAZE"  
+    files { "**.h", "**.cpp" } 
+    location "Maze-AI/MAZE"
+    kind "staticlib"
+    -- [AppMode Debug]
+    filter "configurations:Debug"
+    defines { "CE_DEBUG" }
+    symbols "On"
+    optimize "Off"
+    targetdir "Output/Debug" 
+    debugdir "Output/Debug"
+    -- [AppMode Release]
+    filter "configurations:Release"
+    defines { "CE_RELEASE" }
     symbols "Off"
     optimize "On"
     targetdir "Output/Release" 
